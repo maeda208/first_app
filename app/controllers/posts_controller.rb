8f9cbb4635e18b0faac425c7d6d085904c0254f4
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-05-19 03:17:42 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all# 1番目のレコードを@postに代入
+    
   end
 
-  def new
-  end
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to "/posts"
-  end
 end
